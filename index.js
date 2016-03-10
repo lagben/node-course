c9bcb45a60555d28f935ae0b6a6b3e8ba985933a
fibelage <benjamin.lager@finn.no>
2016-03-10 10:29:39 +0100

    added simple server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("i'm using intellij");
+var http = require('http');
+
+console.log('hello world');
+
+var server = http.createServer(function(req, res) {
+    res.end('hello world');
+});
+
+server.listen(3000, function() {
+    console.log("When this callback is invoked our server is listening on port: " + 3000);
+});
